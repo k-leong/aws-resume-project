031676de185fc83ef22553d93b0be33d756248f7
k-leong <kenny.leong0221@yahoo.com>
2025-05-11 19:06:01 -0700

    changed links to open in new tab

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,12 +1,12 @@
-output "apigateway_url" {
-  value = module.apigateway.apigateway_url
-  sensitive = true
-}
+# output "apigateway_url" {
+#   value = module.apigateway.apigateway_url
+#   sensitive = true
+# }
 
-output "apigateway_route" {
-  value = module.apigateway.apigateway_route
-  sensitive = true
-}
+# output "apigateway_route" {
+#   value = module.apigateway.apigateway_route
+#   sensitive = true
+# }
 
 # output "aws_s3_bucket" {
 #   value = module.s3.aws_s3_bucket
